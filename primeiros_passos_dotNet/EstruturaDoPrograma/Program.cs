ddd7cf9f9c9426e2671c50361edf8508a64188a8
Thiago Oliveira <thiagop_o@hotmail.com>
2021-06-11 05:25:32 -0300

    Terminado o Bloco Conhecendo o C#

---
+++ primeiros_passos_dotNet/EstruturaDoPrograma/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EstruturaDoPrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Pilha();
+            s.Empilha(1);
+            s.Empilha(19);
+            s.Empilha(100);
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+        }
+    }
+}
